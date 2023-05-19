3e7576f7a2b25ca9489af04b5eb27bab62a2bc6b
Zoe Codda <coddazoe@gmail.com>
2023-05-19 16:55:30 +0200

    front welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="logo_welcome.png"className="Catch a Ball" alt="Catch a Ball" />
+        <img src="pok3.png"className="three pokemons" alt="three pokemons" />
+      <div className='buttonsAtBtm'>
+      <img src="register.png"className="register" alt="register" />
+      <img src="login.png"className="login" alt="login" />
+
+
+
+      </div>
       </header>
+   
+
     </div>
   );
 }

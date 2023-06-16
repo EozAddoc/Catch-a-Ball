619b3b839a7ef330077a1c008b8a6e47118c55c5
julie17-d <derbaljulie@gmail.com>
2023-06-16 16:56:57 +0200

    test api call pokemon card: clean up, comments, and added back of pkmn cards in public

--- client/src/pages/TestApi.jsx
+++ client/src/pages/TestApi.jsx
@@ -4,17 +4,21 @@ import { useState, useEffect } from 'react';
 function TestApi() {
     pokemon.configure({ apiKey: 'fd4c85a9-6be2-4277-a624-884003a17516' });
     const [card, setCard] = useState({});
-    useEffect(() => {
-        pokemon.card.find('base1-4') // base1-4 = Charizard
+
+    useEffect(() => {   // setCard(card) must be in a useEffect so it doesn't call the
+                        // API over and over
+
+        pokemon.card.find('base1-6')    // base1-4 = Charizard; get a specific
+                                        // card with its id
             .then(card => {
                 console.log(card);
                 setCard(card);
             });
-    }, []);
+    }, []); // empty array for useEffect so it only renders once
 
     return (
         <div>
-            <img src={card.images?.large ?? "pok3.png"} />
+            <img src={card.images?.large ?? "pkmn-cardback.png"} />
         </div>
     )
 }

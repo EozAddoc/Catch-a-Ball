3ce4caca27c9e2934791a0b547c071b3a0fa5773
Zoe Codda <coddazoe@gmail.com>
2023-07-08 19:36:10 +0200

    fix conflicts

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -3,22 +3,22 @@ import Form from "../components/Form";
 
 function RegisterPage() {
   return (
-    <div className="flex h-screen">
-      <div className="flex-1  w-[20%] relative h-screen bg-custom-pokeB">
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="md:w-1/2 relative bg-custom-pokeB hidden md:block">
         <img
           src="register_img.jpg"
           alt="registerImg"
           className="opacity-50 w-full h-full object-cover"
         />
       </div>
-      <div className="flex-1 w-[80%] bg-custom-pokeB flex flex-col">
-        <div className="flex-grow h-[25%]">
+      <div className="md:w-1/2 bg-custom-pokeB flex flex-col">
+        <div className="md:h-1/4">
           <div className="m-5 flex items-center justify-center">
             <img src="register.png" alt="RegisterImage" />
           </div>
         </div>
-        <div className="flex-grow h-[75%] bg-custom-pokeB">
-          <Form></Form>
+        <div className="md:h-3/4 bg-custom-pokeB">
+          <Form />
         </div>
       </div>
     </div>

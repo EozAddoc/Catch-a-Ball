3fccf8b47ce560521739d98105515b2c368e13a3
Zoe Codda <coddazoe@gmail.com>
2023-07-08 19:31:18 +0200

    finally finished front of register and login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,80 +2,25 @@ import Form from "../components/Form";
 
 function LoginPage() {
   return (
-    <div className="flex min-h-screen  bg-custom-pokeB">
-      <div className="w-2/5 h-30 flex mx-0 relative">
-        <img
-          src="login_img.jpg"
-          alt="loginImg"
-          className="opacity-50 w-full h-auto "
-        />
+    <div className="flex h-screen">
+    <div className="flex-1  w-[20%] relative h-screen bg-custom-pokeB">
+      <img
+        src="login_img.jpg"
+        alt="logImg"
+        className="opacity-50 w-full h-full object-cover"
+      />
+    </div>
+    <div className="flex-1 w-[80%] bg-custom-pokeB flex flex-col">
+      <div className="flex-grow h-[25%]">
+        <div className="m-5 flex items-center justify-center">
+          <img src="login.png" alt="LoginImage" />
+        </div>
       </div>
-      <div className="  m-5 flex flex-col justify-center items-center">
-     
-<div
-  class="container max-w-md mx-auto xl:max-w-3xl  flex bg-white rounded-lg shadow overflow-hidden"
->
-
-  <div class="w-full  p-8">
-    <form method="post" action="#" onSubmit="return false">
-      <h1 class=" text-2xl font-bold">Sign in to your account</h1>
-      <div>
-        <span class="text-gray-600 text-sm">
-          Don't have an account?
-        </span>
-        <span class="text-gray-700 text-sm font-semibold">
-          Sign up
-        </span>
-      </div>
-      <div class="mb-4 mt-6">
-        <label
-          class="block text-gray-700 text-sm font-semibold mb-2"
-          htmlFor="email"
-        >
-          Email
-        </label>
-        <input
-          class="text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10"
-          id="email"
-          type="text"
-          placeholder="Your email address"
-        />
-      </div>
-      <div class="mb-6 mt-6">
-        <label
-          class="block text-gray-700 text-sm font-semibold mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <input
-          class="text-sm bg-gray-200 appearance-none rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline h-10"
-          id="password"
-          type="password"
-          placeholder="Your password"
-        />
-        <a
-          class="inline-block align-baseline text-sm text-gray-600 hover:text-gray-800"
-          href="/forgot"
-        >
-          Forgot Password?
-        </a>
-      </div>
-      <div class="flex w-full mt-8">
-        <button
-          class="w-full bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
-          type="button"
-        >
-          Sign in
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
-          
-       
+      <div className="flex-grow h-[75%] bg-custom-pokeB">
+        <Form></Form>
       </div>
     </div>
+  </div>
   ); 
 }
 
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import Form from '../components/Form';
+import React from "react";
+import Form from "../components/Form";
 
 function RegisterPage() {
   return (
-    <div class="flex min-h-screen bg-black">
-      <Form />
+    <div className="flex h-screen">
+      <div className="flex-1  w-[20%] relative h-screen bg-custom-pokeB">
+        <img
+          src="register_img.jpg"
+          alt="registerImg"
+          className="opacity-50 w-full h-full object-cover"
+        />
+      </div>
+      <div className="flex-1 w-[80%] bg-custom-pokeB flex flex-col">
+        <div className="flex-grow h-[25%]">
+          <div className="m-5 flex items-center justify-center">
+            <img src="register.png" alt="RegisterImage" />
+          </div>
+        </div>
+        <div className="flex-grow h-[75%] bg-custom-pokeB">
+          <Form></Form>
+        </div>
+      </div>
     </div>
   );
 }

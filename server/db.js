272a973f78d8ee8df7df11e1b0dc902e790b4d51
Zoe Codda <coddazoe@gmail.com>
2023-09-22 17:36:03 +0200

    try

--- server/db.js
+++ server/db.js
@@ -1,18 +1,39 @@
+// const mysql = require('mysql');
+// require('dotenv').config();
+
+// const db = mysql.createConnection({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to the database:', err.message);
+//   } else {
+//     console.log('Connected to the database.');
+//   }
+// });
 const mysql = require('mysql');
 require('dotenv').config();
 
+
 const db = mysql.createConnection({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  user:'root',
+  password:"rootpw",
+  database:"CatchABall",
+  host:"db",
+  port: '3306',
+  multipleStatements: true
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.message);
+    console.error('Error connecting to the database:', err.message, db.user);
   } else {
     console.log('Connected to the database.');
   }
 });
+
 
 module.exports = db;

3c0335bffdbaae7d5f32dd9f9adaef80ac396e2d
Zoe Codda <coddazoe@gmail.com>
2023-10-13 16:13:31 +0200

    wairt

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,98 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
-import pokemon from 'pokemontcgsdk';
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+import pokemon from 'pokemontcgsdk'
 import LoadingPage from '../LoadingPage';
-import ProfileCard from '../components/ProfileCard';
-import Sidebar from '../components/SideBar';
+
+import Sidebar from "../components/SideBar";
+
 
 async function ApiCall(id) {
-  pokemon.configure({ apiKey: process.env.REACT_APP_API_KEY });
-  const card = await pokemon.card.find(id);
-  return card;
+    pokemon.configure({ apiKey: process.env.REACT_APP_API_KEY });
+
+    const card = await pokemon.card.find(id);
+
+    return card;
 }
 
 function Home() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(false);
-  const [mess, setMess] = useState('');
-  const [avatar, setAvatar] = useState('');
-  const [userData, setUserData] = useState('');
-  const [deckData, setDeckData] = useState([]);
-  const [deckInfo, setDeckInfo] = useState([]);
-  const [isLoadingData, setIsLoadingData] = useState(true);
+    const navigate = useNavigate();
+    const [auth, setAuth] = useState(false);
+    const [mess, setMess] = useState('');
+    const [avatar, setAvatar] = useState('');
+    const [userData, setUserData] = useState('');
+    const [deckData, setDeckData] = useState([]);
+    const [deckInfo, setDeckInfo] = useState([]);
+    const [isLoadingData, setIsLoadingData] = useState(true);
 
-  axios.defaults.withCredentials = true;
+    axios.defaults.withCredentials = true;
 
-  useEffect(() => {
-    axios.get(`http://${process.env.REACT_APP_URL}:1117/user`)
-      .then(res => {
-        if (res.data.Status === 'Success') {
-          setAuth(true);
-          setUserData(res.data.userData);
-        } else {
-          setAuth(false);
-          setMess(res.data.err);
-        }
-      })
-      .catch(err => console.log('error', err));
-  }, []);
+    useEffect(() => {
+        axios.get('http://' + process.env.REACT_APP_URL + ':1117/user')
+            .then(res => {
+                if (res.data.Status === "Success") {
+                    setAuth(true);
+                    setUserData(res.data.userData);
+                } else {
+                    setAuth(false);
+                    setMess(res.data.err);
+                }
+            })
+            .catch(err => console.log("error", err));
+    }, []);
 
-  useEffect(() => {
-    axios.get(`http://${process.env.REACT_APP_URL}:1117/deck`)
-      .then(res => {
-        if (res.data.Status === 'Success') {
-          setDeckData(res.data.deckData);
-        } else {
-          setMess(res.data.err);
-        }
-      })
-      .catch(err => console.log('error', err));
-  }, []);
+    useEffect(() => {
+        axios.get('http://' + process.env.REACT_APP_URL + ':1117/deck')
+            .then(res => {
+                if (res.data.Status === "Success") {
+                    setDeckData(res.data.deckData);
+                } else {
+                    setMess(res.data.err);
+                }
+            })
+            .catch(err => console.log("error", err));
+    }, []);
 
-  useEffect(() => {
-    deckData.forEach(async (deckItem) => {
-      const data = await ApiCall(deckItem.card_api);
-      setDeckInfo((value) => [...value, data]);
-      setAvatar(await ApiCall(userData.avatar_api));
-    });
-  }, [deckData]);
+    useEffect(() => {
+        deckData.map(async (deckItem) => {
+            const data = await ApiCall(deckItem.card_api);
+            setDeckInfo((value) => [...value, data]);
+            setAvatar(await ApiCall(userData.avatar_api));
+        });
+    }, [deckData]);
 
-  return (
-    <div className="bg-gray-700">
-      {auth ? (
-        <div className="min-h-screen bg-home bg-cover opacity-100">
-          <div className="flex">
-            <div className="fixed top-0 right-0 p-4 h-1/5 w-1/3 text-white">
-              <ProfileCard />
-            </div>
-          </div>
+    return (
+        <div className='bg-gray-700'>
+            {
+                auth ?
+                    <div class="min-h-screen min-w-screen bg-home bg-cover opacity-100">
+                        <div className="text-center min-h-screen px-5 py-5">
+                            <div className='flex'>
+                                <div className='bg-[#070741] mx-[0px] md:mx-[150px] lg:mx-[300px] mb-2 md:mb-5 rounded-lg px-5 py-7 text-white'>
+                                    <h2 className='text-5xl lg:text-8xl tracking-wide font-pixel'>YOUR DECK</h2>
+                                </div>
+                                <div className='text-white font-pixel flex items-center bg-[#070741] rounded-full'>
+                                    <div className='mx-4 mt-16px'>
+                                        <h3>{userData.username}</h3>
+                                        <h4>LVL {userData.lvl}</h4>
+                                    </div>
+                                    <img className="h-24 rotate-6" src={avatar.images?.large} alt={avatar.name} />
+                                </div>
+                            </div>
+                            <div className='grid grid-cols-2 md:grid-cols-3 place-items-center'>
+                                {deckInfo.length > 0 && deckInfo.map((card) => {
+                                    return (
+                                        <img className="hover:scale-150 transition w-36 p-2 md:w-64" src={card.images?.large} alt={card.name} />
+                                    )
+                                })}
+                            </div>
+                            <Sidebar />
+                        </div>
+                    </div>
+                    : navigate('/')
+            }
 
-          <Sidebar />
-          <div className="flex items-center justify-center h-screen">
-            <div className="w-1/2 h-32 bg-blue-800 font-bold font-si text-white p-4 rounded-md shadow-md">
-            <div className="w-full h-full">
-      <span className="text-white  text-2xl font-inter font-semibold italic break-words">
-        Win 3 battles in the battle arena
-      </span>
-      <span className="text-white text-2xl font-inter font-semibold break-words">
-        {'              '}
-      </span>
-      <span className="text-yellow-400 text-lg  font-inter font-semibold break-words">
-        rewards 100 xp <br />
-      </span>
-      <span className="text-white text-sm font-inter font-semibold break-words">
-        {' '}
-      </span>
-    </div>            </div>
-          </div>
         </div>
-      ) : (
-        navigate('/')
-      )}
-    </div>
-  );
+    )
 }
-
 export default Home;

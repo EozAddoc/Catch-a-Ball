933cdd02aecc1cfe6744dc1b5768d717c09485cf
julie17-d <derbaljulie@gmail.com>
2024-02-22 13:25:28 +0100

    style: responsive search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -22,44 +22,41 @@ function Search() {
   return (
     <div className="bg-blue-950">
       <div className="bg-townNN bg-cover h-screen flex flex-col items-center justify-center">
-        <div className="search w-3/5 ml-10 fixed top-24 ">
-          {/* Don't forget to pass searchResults as a prop to SearchBar */}
+        <div className="search w-1/2 mb-8 fixed top-20 mr-5">
           <Searchbar searchResults={searchResults} />
         </div>
-        <div className="w-3/5 ml-30 mt-36 bg-transparent">
+        <div className="w-4/5">
           <h2 className="text-white font-bold justify-center text-center m-10 ">
-            {searchResults.length}&nbsp; R E S U L T S :{' '}
+            {searchResults.length} &nbsp; R&nbsp;E&nbsp;S&nbsp;U&nbsp;L&nbsp;T&nbsp;S&nbsp;:
           </h2>
-          <div className="w-full">
-          <div className="max-h-96 overflow-y-auto   p-4">
+          <div className="flex flex-col gap-10">
             {searchResults.map((result, index) => (
-              <div key={index} className="flex w-full">
+              <div key={index} className="flex flex-col lg:flex-row lg:gap-10 lg:items-center lg:justify-center w-full">
                 <div
                   style={{ backgroundColor: getRandomColor(index), letterSpacing: '0.2em' }}
-                  className="rounded-full  relative p-1 m-10  w-full h-14 flex text-white font-semibold mt-3 tracking-wide"
+                  className="rounded-full text-center flex items-center text-white font-semibold lg:w-3/5 tracking-wide"
                 >
                   <div className="flex-1">
-                    <div className="bg-white ml-2 rounded-full w-12 h-12">
+                    <div className="bg-white m-1 rounded-full w-12 h-12">
                       {/* Content of the circular div */}
                     </div>
                   </div>
-                  <div className="flex-1 mt-3">
-                    <p>{result.username}</p>
+                  <div className="flex-1">
+                    {result.username}
                   </div>
-                  <div className="flex-1 text-white font-semibold mt-3 w-1/6 tracking-wide">
-                    L V L {result.lvl}
+                  <div className="flex-1 text-white font-semibold tracking-wide">
+                    L&nbsp;V&nbsp;L {result.lvl}
                   </div>
                 </div>
-                <div className="bg-yellow-300 text-center font-bold text-xl rounded-full ml-10 mt-10 ">
+                <div className="bg-yellow-300 text-center font-bold text-xl rounded-full mt-10 lg:m-0 lg:w-1/5">
                   <div className="m-2">
-                  <button onClick={() => Battle(result.id)}>
-                                          <p className="m-3">B A T T L E</p>
+                    <button onClick={() => Battle(result.id)}>
+                      <p className="m-3">B&nbsp;A&nbsp;T&nbsp;T&nbsp;L&nbsp;E</p>
                     </button>
                   </div>
                 </div>
               </div>
             ))}
-          </div>
           </div>
         </div>
       </div>

e0a197d4de77ccdb2405ebee418162e404c39e31
julie17-d <derbaljulie@gmail.com>
2024-03-03 12:11:47 +0100

    added content security policy

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.set("Content-Security-Policy", "default-src 'none'; img-src 'self'");
+  res.set("Content-Security-Policy", "default-src 'none'; img-src 'self' data:");
   next()
 })
 
